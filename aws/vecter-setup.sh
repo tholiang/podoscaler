84005fddccc6435288b4c5288e29a84dc7c1be90
kevinz13 <kevinz13@illinois.edu>
2025-05-04 11:02:26 -0500

    fix region

--- aws/vecter-setup.sh
+++ aws/vecter-setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # cloudwatch secrets
-kubectl create secret generic aws-secrets --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --from-literal=AWS_REGION=$AWS_REGION
+kubectl create secret generic aws-secrets --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY --from-literal=AWS_REGION=$REGION
 
 # install metrics server
 kubectl apply -f ~/setup/components.yaml
